924229a1d7ea60ba89596b48edbfd0635e7399c9
Cameron Day <32853335+ConanDW@users.noreply.github.com>
2024-02-22 14:50:46 -0500

    Update zabbix.ps1
    - CWKhristos refactored and cleaned up code.
    - Added checks to make sure log file is correct.
    - Fixed output formatting
    - Added more tags for the MSIEXEC config.

--- zabbix.ps1
+++ zabbix.ps1
@@ -1,3 +1,4 @@
+#FIX LINE # WHEN SCRIPT IS DONE CAMERON
 <#
     .SYNOPSIS
         Monitoring - Windows - Zabbix - Cameron Day - Chris Bledsoe
@@ -9,20 +10,23 @@
         2024-02-20: Initial version
 #>
 #region - DECLORATIONS
-$script:diag = $null
-$script:blnWARN = $false
-$script:blnBREAK = $false
-$script:zabbixInstalled = Get-Service "Zabbix Agent 2" -ErrorAction SilentlyContinue
-$script:mode = $env:mode
-$script:dattoCustomerName = $env:CS_PROFILE_NAME
-$script:hostname = $env:COMPUTERNAME
-$script:serverHostnameOrIP = $env:serverHostnameOrIP
-$script:pskID = $env:pskID
-$script:pskValue = $env:pskValue
-$installFolder = "C:\IT\Zabbix"
-$logFile = "C:\IT\Log\Zabbix.log"
-$zabbixDownload = "https://cdn.zabbix.com/zabbix/binaries/stable/6.4/6.4.11/zabbix_agent2-6.4.11-windows-amd64-openssl.msi"
-$strLineSeparator = "----------------------------------"
+$script:diag                = $null
+$script:blnWARN             = $false
+$script:blnBREAK            = $false
+$script:mode                = $env:mode
+$script:pskID               = $env:pskID
+$script:pskValue            = $env:pskValue
+$script:hostname            = $env:COMPUTERNAME
+$script:ActivePassive       = $env:ActivePassive
+$script:dattoCustomerName   = $env:CS_PROFILE_NAME
+$script:serverHostnameOrIP  = $env:serverHostnameOrIP
+$script:zabbixInstalled     = Get-Service "Zabbix Agent 2" -ErrorAction SilentlyContinue
+$installFolder              = "C:\IT\Zabbix"
+$logPath                    = "C:\IT\Log\Zabbix_Monitor"
+$logFile                    = "C:\IT\Log\Zabbix_install.log"
+$pkg                        = "C:\IT\Zabbix_Agent_2.msi"
+$zabbixDownload             = "https://cdn.zabbix.com/zabbix/binaries/stable/6.4/6.4.11/zabbix_agent2-6.4.11-windows-amd64-openssl.msi"
+$strLineSeparator           = "----------------------------------"
 #endregion - DECLORATIONS
 
 #region - FUNCTIONS
@@ -46,6 +50,7 @@ function StopClock {
   $Minutes = $sw.Elapsed.Minutes
   $Seconds = $sw.Elapsed.Seconds
   $Milliseconds = $sw.Elapsed.Milliseconds
+  $script:finish = (get-date -format "yyyy-MM-dd HH:mm:ss").ToString()
   $ScriptStopTime = (get-date -format "yyyy-MM-dd HH:mm:ss").ToString()
   write-output "`r`nTotal Execution Time - $($Minutes) Minutes : $($Seconds) Seconds : $($Milliseconds) Milliseconds`r`n"
   $script:diag += "`r`n`r`nTotal Execution Time - $($Minutes) Minutes : $($Seconds) Seconds : $($Milliseconds) Milliseconds`r`n"
@@ -97,10 +102,7 @@ function dir-Check () {
 }
 
 function run-Download {
-  logERR 3 "Starting deploy"
-  $pkg = "C:\IT\Zabbix_Agent_2.msi"
-  $taskdiag = "Downloading and Deploying Zabbix 2`r`n$($strLineSeparator)"
-  logERR 3 "Downloading Zabbix" "Line 88 - $($taskdiag)"
+  logERR 3 "run-Download" "Line 88 - Downloading Zabbix 2`r`n$($strLineSeparator)"
   try { Invoke-WebRequest -uri "$($zabbixDownload)" -OutFile "$($pkg)" } catch {
     $err = "$($_.Exception)`r`n$($_.scriptstacktrace)`r`n$($_)"
     $taskdiag = "Failed to Download Zabbix 2`r`n$($strLineSeparator)"
@@ -110,107 +112,169 @@ function run-Download {
 
 function run-Deploy {
   try {
+    #OS Type & Version
+    $osarch = (Get-WmiObject -class Win32_OperatingSystem)
+    Switch ($osarch.ProductType) {
+      "1" {$producttype = "Workstation"}
+      "2" {$producttype = "DC"}
+      "3" {$producttype = "Server"}
+    }
+    logERR 3 "run-Deploy" "Deploying Zabbix 2`r`n$($strLineSeparator)"
     $params = "/l*v $($logFile) "
-    $params += "HOSTNAME=`"$($script:dattoCustomerName) - $($script:hostname)`" LISTENPORT=10050 ENABLEPATH=1 "
-    #$params += "HOSTMETADATA=windows"
-    $params += "SERVER=$($script:serverHostnameOrIP) SERVERACTIVE=$($script:serverHostnameOrIP) INSTALLFOLDER=`"$($installFolder)`" "
+    $params += "HOSTNAME=`"$($script:dattoCustomerName) - $($script:hostname)`" ENABLEPATH=1 HOSTMETADATA=Windows$($producttype):$($script:ActivePassive) "
+    $params += "LISTENPORT=10050 SERVER=$($script:serverHostnameOrIP) SERVERACTIVE=$($script:serverHostnameOrIP) INSTALLFOLDER=`"$($installFolder)`" "
     $params += "TLSCONNECT=psk TLSACCEPT=psk TLSPSKIDENTITY=$($script:pskID) TLSPSKVALUE=$($script:pskValue) /qn"
     $taskdiag = Start-Process -FilePath $pkg -ArgumentList "$($params)" -PassThru -Wait
+    logERR 3 "run-Deploy" "Deploy Complete :`r`n$($taskdiag)`r`n$($strLineSeparator)"
   } catch {
     $err = "$($_.Exception)`r`n$($_.scriptstacktrace)`r`n$($_)"
-    $taskdiag += "`r`nFailed to Install Zabbix 2`r`n$($strLineSeparator)"
+    $taskdiag += "Failed to Install Zabbix 2`r`n$($strLineSeparator)"
     logERR 2 "run-Deploy" "Line 106 - $($taskdiag)`r`n$($err)`r`n$($strLineSeparator)"
   }
   Start-Sleep -seconds 30
 }
 
 function run-Remove {
-  $taskdiag += "`r`nRemoving Zabbix`r`n$($strLineSeparator)"
-  logERR 3 "run-Remove" "Line 135 - $($taskdiag)"
+  logERR 3 "run-Remove" "Line 135 - Removing Zabbix`r`n$($strLineSeparator)"
   $regPath = get-itemproperty -path 'HKLM:\SOFTWARE\Zabbix SIA\Zabbix Agent 2 (64-bit)' -name 'ProductCode'
   $regPath.ProductCode
   Start-Process -FilePath "C:\Windows\system32\msiexec.exe" -ArgumentList "/x $($regPath.ProductCode) /qn" -PassThru -Wait
-  rm $installFolder -Recurse -Force
+  rm $installFolder -Recurse -Force -SilentlyContinue
+}
+
+function run-Upgrade () {
+  try {
+    run-Remove -wait
+  } catch {
+    $err = "$($_.Exception)`r`n$($_.scriptstacktrace)`r`n$($_)"
+    $taskdiag += "`r`nFailed to Remove Zabbix 2`r`n$($strLineSeparator)"
+    logERR 2 "run-Upgrade" "Line 106 - $($taskdiag)`r`n$($err)`r`n$($strLineSeparator)"
+  }
+  Start-Sleep -Seconds 2
+  try {
+    run-Download -wait
+    run-Deploy -wait
+  } catch {
+    $err = "$($_.Exception)`r`n$($_.scriptstacktrace)`r`n$($_)"
+    $taskdiag += "`r`nFailed to Upgrade Zabbix 2`r`n$($strLineSeparator)"
+    logERR 2 "run-Upgrade" "Line 106 - $($taskdiag)`r`n$($err)`r`n$($strLineSeparator)"
+  }
 }
 
 function run-Monitor {
   if ($($script:zabbixInstalled.Status) -eq "Stopped") {
     $taskdiag = "Warning! Service is not started : `r`nAttempting to start`r`n$($strLineSeparator)"
-    logERR 3 "Monitor Mode" "Line 153 - $($taskdiag)"
+    logERR 3 "run-Monitor" "Line 153 - $($taskdiag)"
     try {Start-Service -Name $($script:zabbixInstalled.Name)} catch {
       $err = "$($_.Exception)`r`n$($_.scriptstacktrace)`r`n$($_)"
       $taskdiag += "`r`nFailed to Start Zabbix Service`r`n$($strLineSeparator)"
-      logERR 2 "Monitor Mode" "Line 157 - $($taskdiag)`r`n$($err)`r`n$($strLineSeparator)"
+      logERR 2 "run-Monitor" "Line 157 - $($taskdiag)`r`n$($err)`r`n$($strLineSeparator)"
     }
     #& "c:\IT\Zabbix\zabbix_agent2.exe -c c:\IT\Zabbix\zabbix_agent2.conf -i"
   }
-  if ($($script:zabbixInstalled.Status) -eq "Running") { 
-    $taskdiag = "$($script:zabbixInstalled.Name) is started : `r`nAttempting to start`r`n$($strLineSeparator)"
-    logERR 3 "Monitor Mode" "Line 163 - $($taskdiag)" 
-    
+  if (-not ($script:blnBREAK)) {
+    if ($($script:zabbixInstalled.Status) -eq "Running") { 
+      $taskdiag = "$($script:zabbixInstalled.Name) is started`r`n$($strLineSeparator)"
+      logERR 3 "run-Monitor" "Line 163 - $($taskdiag)" 
+    }
+    $taskdiag = "Begin Verification of Correct Configuration : `r`n$($strLineSeparator)"
+    logERR 3 "run-Monitor" "Line 166 - $($taskdiag)" 
+    #verify ServerIP, and customername & devicename in proper format : '[CustomerName - DeviceName]'
+    $blnMatch = $true
+    $localConfig = Get-Content -Path "C:\IT\Zabbix\zabbix_agent2.conf"
+    if (-not ($localConfig -match "Hostname=$($script:dattoCustomerName) - $($script:hostname)")) {
+      $blnMatch = $false
+      logERR 3 "run-Monitor" "Config doesn't match hostname"
+    }
+    if (-not ($localConfig -match "Server=$($script:serverHostnameOrIP)")) {
+      $blnMatch = $false
+      logERR 3 "run-Monitor" "Config doesn't match server hostname or IP"
+    }
+    if (-not ($localConfig -match "ServerActive=$($script:serverHostnameOrIP)")) {
+      $blnMatch = $false
+      logERR 3 "run-Monitor" "Config doesn't match server (active) hostname or IP"
+    }
+    if (-not ($localConfig -match "$($script:pskID)")) {
+      $blnMatch = $false
+      logERR 3 "run-Monitor" "PSK ID doesn't match PSK Identity"
+    }
+    $pskFile = Get-Content -Path "C:\IT\Zabbix\psk.key"
+    if (-not ($pskFile -match "$($script:pskValue)")) {
+      $blnMatch = $false
+      logERR 3 "run-Monitor" "PSK value doesn't match psk.key"
+    }
+    if (-not ($blnMatch)) { run-Upgrade }
   }
-  $taskdiag = "Begin Verification of Correct Configuration : `r`n$($strLineSeparator)"
-  logERR 3 "Monitor Mode" "Line 166 - $($taskdiag)" 
-  <#If ($params) {
-    
-  }#>
 }
 #endregion - FUNCTIONS
 
 #region - SCRIPT
 $ScrptStartTime = (get-date -format "yyyy-MM-dd HH:mm:ss").ToString()
 $script:sw = [Diagnostics.Stopwatch]::StartNew()
-dir-Check
+dir-Check -wait
 switch ($script:mode) {
   "Deploy" {
     $taskdiag = "Downloading Zabbix 2`r`n$($strLineSeparator)"
-    logERR 3 "run-Download" "Line 168 - $($taskdiag)"
-    run-Download
+    logERR 3 "Mode : $($script:mode)" "Line 168 - $($taskdiag)"
+    run-Download -wait
     $taskdiag = "Deploying Zabbix 2`r`n$($strLineSeparator)"
-    logERR 3 "run-Deploy" "Line 171 - $($taskdiag)"
-    run-Deploy
-    $script:zabbixInstalled = Get-Service "Zabbix Agent 2" 
-    Restart-Service -Name $($script:zabbixInstalled.Name)
-    if ($($script:zabbixInstalled.Status) -eq "Stopped") {
+    logERR 3 "Mode : $($script:mode)" "Line 171 - $($taskdiag)"
+    run-Deploy -wait
+    $script:zabbixInstalled = Get-Service "Zabbix Agent 2"
+    if ($($script:zabbixInstalled.Status) -ne "Running") {
+      Restart-Service -Name $($script:zabbixInstalled.Name)
       $taskdiag = "Warning! Service is not started : `r`nAttempting to start`r`n$($strLineSeparator)"
-      logERR 3 "Deploy Mode" "Line 177 - $($taskdiag)"
+      logERR 3 "Mode : $($script:mode)" "Line 177 - $($taskdiag)"
       #Write-Warning "Service is not started : `r`n Attempting to start"
       Start-Service -Name $($script:zabbixInstalled.Name)
       #& "c:\IT\Zabbix\zabbix_agent2.exe -c c:\IT\Zabbix\zabbix_agent2.conf -i"
     }
     if ($($script:zabbixInstalled.Status) -eq "Running") { Write-Output "Zabbix is running" }
-    $strOUT = $taskdiag
-    write-DRMMAlert "$($strOUT)"
-    write-DRMMDiag "$($strOUT)`r`n$($strModule)`r`n$($strErr)"
   }
   "Remove" {
-    run-Remove
-    $strOUT = $taskdiag
-    write-DRMMAlert "$($strOUT)"
-    write-DRMMDiag "$($strOUT)`r`n$($strModule)`r`n$($strErr)"
+    run-Remove -wait
   }
   "Upgrade" {
-    run-Remove -wait
-    Start-Sleep -Seconds 2
-    run-Download -wait
-    run-Deploy
-    $strOUT = $taskdiag
-    write-DRMMAlert "$($strOUT)"
-    write-DRMMDiag "$($strOUT)`r`n$($strModule)`r`n$($strErr)"
+    run-Upgrade -wait
   }
   "Monitor" {
-    dir-Check
     if (-not (test-path -path "C:\IT\Zabbix")) { 
       $taskdiag = "No C:\IT\Zabbix dir Means that Zabbix was not Installed Correctly : Rerunning Download/Deploy`r`n$($strLineSeparator)"
-      logERR 3 "Monitor Mode" "Line 213 - $($taskdiag)"
+      logERR 3 "Mode : $($script:mode)" "Line 213 - $($taskdiag)"
       run-Download
       run-Deploy 
     }
     run-Monitor
-    $strOUT = $taskdiag
-    write-DRMMAlert "$($strOUT)"
-    write-DRMMDiag "$($strOUT)`r`n$($strModule)`r`n$($strErr)"
   }
 }
+
+$script:finish = (get-date -format "yyyy-MM-dd HH:mm:ss").ToString()
+if (-not $script:blnBREAK) {
+  if (-not $script:blnWARN) {
+    $enddiag = "Execution Successful : $($script:finish)`r`n$($strLineSeparator)`r`n"
+    logERR 3 "END" "$($enddiag)"
+    #WRITE TO LOGFILE
+    "$($script:diag)" | add-content $logPath -force
+    write-DRMMAlert "$($script:mode) : Healthy : $($script:finish)"
+    write-DRMMDiag "$($script:diag)"
+    exit 1
+  } elseif ($script:blnWARN) {
+    $enddiag = "Execution Completed with Warnings : $($script:finish)`r`n$($strLineSeparator)`r`n"
+    logERR 3 "END" "$($enddiag)"
+    #WRITE TO LOGFILE
+    "$($script:diag)" | add-content $logPath -force
+    write-DRMMAlert "$($script:mode) : Execution Completed with Warnings : Diagnostics - $($logPath) : $($script:finish)"
+    write-DRMMDiag "$($script:diag)"
+    exit 0
+  }
+} elseif ($script:blnBREAK) {
+  #WRITE TO LOGFILE
+  $enddiag += "Execution Failed : $($finish)`r`n$($strLineSeparator)"
+  logERR 4 "END" "$($enddiag)"
+  "$($script:diag)" | add-content $logPath -force
+  write-DRMMAlert "$($script:mode) : Failure : Diagnostics - $($logPath) : $($script:finish)"
+  write-DRMMDiag "$($script:diag)"
+  exit 1
+}
 #endregion - SCRIPT
-#check if dir is there, if service is there, check config file and make sure the hostname string and server string match our expectates.
+#FIX LINE # WHEN SCRIPT IS DONE CAMERON
